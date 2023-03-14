2e261e9aa50beef730a43c98a005c804ce40eec5
mayowadavid <mayowadavid100@gmail.com>
2023-03-14 22:02:07 +0100

    update matcher

--- middleware.js
+++ middleware.js
@@ -6,14 +6,13 @@ export function middleware(req, ev) {
   const host = req.headers.get('host');
   
   if (host && host.startsWith('blog.')) {
-    console.log(host)
     return NextResponse.redirect(`/blog`, { status: 301 });
   } else {
     return NextResponse.next();
   }
 }
 
-export const config = {
-  matcher: "/"
-}
+// export const config = {
+//   matcher: "/"
+// }
 
